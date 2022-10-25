24662e310d7acc290d5431f5dbb31700ecb87339
Ruoxin <liruoxin@hyperchain.cn>
2022-10-25 17:02:40 +0800

    fix:update packr2

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -7,7 +7,21 @@ function print_blue() {
   printf "${BLUE}%s${NC}\n" "$1"
 }
 
+function go_install() {
+  version=$(go env GOVERSION)
+  if [[ ! "$version" < "go1.16" ]];then
+      go install "$@"
+  else
+      go get "$@"
+  fi
+}
+
 print_blue "===> 1. Install golangci-lint"
 if ! type golanci-lint >/dev/null 2>&1; then
-  go get github.com/golangci/golangci-lint/cmd/golangci-lint@v1.23.0
+  go_install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.23.0
+fi
+
+print_blue "===> 2. Install packr2"
+if ! type packr2 >/dev/null 2>&1; then
+  go_install github.com/gobuffalo/packr/v2/packr2@v2.8.3
 fi
