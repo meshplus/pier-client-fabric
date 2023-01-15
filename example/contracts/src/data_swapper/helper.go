f7213364132a99a9106ed95fce31626154cb0747
朱沛文 <zhupeiwen@zhupeiwendemacbook-pro.local>
2023-01-15 20:12:21 +0800

    adapt to release-2.8

--- example/contracts/src/data_swapper/helper.go
+++ example/contracts/src/data_swapper/helper.go
@@ -2,40 +2,10 @@ package main
 
 import (
 	"fmt"
-	pb "github.com/hyperledger/fabric/protos/peer"
-	"strconv"
-
 	"github.com/golang/protobuf/proto"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
+	pb "github.com/hyperledger/fabric/protos/peer"
 )
-
-func getUint64(stub shim.ChaincodeStubInterface, key string) (uint64, error) {
-	value, err := stub.GetState(key)
-	if err != nil {
-		return 0, fmt.Errorf("amount must be an interger %w", err)
-	}
-
-	ret, err := strconv.ParseUint(string(value), 10, 64)
-	if err != nil {
-		return 0, err
-	}
-
-	return ret, nil
-}
-
-func getAmountArg(arg string) (uint64, error) {
-	amount, err := strconv.ParseUint(arg, 10, 64)
-	if err != nil {
-		shim.Error(fmt.Errorf("amount must be an interger %w", err).Error())
-		return 0, err
-	}
-
-	if amount < 0 {
-		return 0, fmt.Errorf("amount must be a positive integer, got %s", arg)
-	}
-
-	return amount, nil
-}
 
 func getChaincodeID(stub shim.ChaincodeStubInterface) (string, error) {
 	sp, err := stub.GetSignedProposal()
--- receipt.go
+++ receipt.go
@@ -6,8 +6,18 @@ import (
 )
 
 func (c *Client) generateReceipt(from, to string, idx uint64, args [][]byte, proof []byte, status, encrypt bool, typ uint64) (*pb.IBTP, error) {
-	result := &pb.Result{Data: args}
-	content, err := result.Marshal()
+	var result []*pb.ResultRes
+	res := &pb.ResultRes{Data: args}
+	result = append(result, res)
+
+	var multiStatus []bool
+	if typ == uint64(pb.IBTP_RECEIPT_SUCCESS) {
+		multiStatus = append(multiStatus, true)
+	} else {
+		multiStatus = append(multiStatus, false)
+	}
+	results := &pb.Result{Data: result, MultiStatus: multiStatus}
+	content, err := results.Marshal()
 	if err != nil {
 		return nil, err
 	}
